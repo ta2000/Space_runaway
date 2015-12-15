fbd55b524d2393e7044e41807dd8cc5935d226d2
Tom Andersen <tomandersen2000@gmail.com>
2015-12-15 12:05:18 -0800

    No longer checking number of players constantly

--- js/class.js
+++ js/class.js
@@ -34,27 +34,27 @@ class Player extends Sprite {
 		this.viewDist = 10;
 	}
 
-	var playerNum = entityCount(entities, Player);
+	playerNum : entityCount(entities, Player);
 
 	move(ctx) {
 		/*--W--*/
 		if ( 87 in this.keysDown ) {
-			ctx.translate(0, this.speed/playerNum);
+			ctx.translate(0, this.speed/this.playerNum);
 			this.y-=this.speed;
 		};
 		/*--A--*/
 		if ( 65 in this.keysDown ) {
-			ctx.translate(this.speed/playerNum, 0);
+			ctx.translate(this.speed/this.playerNum, 0);
 			this.x-=this.speed;
 		};
 		/*--S--*/
 		if ( 83 in this.keysDown ) {
-			ctx.translate(0, 0-this.speed/playerNum);
+			ctx.translate(0, 0-this.speed/this.playerNum);
 			this.y+=this.speed;
 		};
 		/*--D--*/
 		if ( 68 in this.keysDown ) {
-			ctx.translate(0-this.speed/playerNum, 0);
+			ctx.translate(0-this.speed/this.playerNum, 0);
 			this.x+=this.speed;
 		};
 	}
