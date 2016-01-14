7a6b8c9be7cb054a872569d5060c9d55087deccc
Tommy Andersen <TAndersen9820@student.pps.net>
2016-01-14 12:50:00 -0800

    No longer using ctx.translate

---
+++ js/Game.js
@@ -0,0 +1,93 @@
+var Game = {
+	levelNum : 1,
+	scale : 32,
+	canvas : document.createElement("canvas"),
+	start : function() {
+		this.canvas.width = window.innerWidth;
+		this.canvas.height = window.innerHeight;
+		this.ctx = this.canvas.getContext("2d");
+		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+		// Key handling
+		window.onkeydown = this.key_down;
+		window.onkeyup = this.key_up;
+		// Level
+		this.loadLevel(Game.levelNum);
+		// Draw
+		Game.draw();
+	},
+	clear : function() {
+		Game.ctx.clearRect(0, 0, Game.canvas.width, Game.canvas.height);
+	},
+	draw : function() {
+		Game.clear();
+		for (var i in entities) {
+			if (entities[i].move!==undefined) {
+				entities[i].move(Game.ctx);
+				Game.ctx.drawImage(Game.canvas, 0, 0);
+			}
+		}
+		Game.clear();
+		for (var i in entities) {
+			entities[i].draw(Game.ctx);
+		}
+		window.requestAnimationFrame(Game.draw);
+	},
+	// Key handling
+	key_down : function(e) {
+		for (var i in entities) {
+			if (entities[i].keysDown!==undefined) {
+				entities[i].keysDown[e.keyCode]=true;
+			}
+		}
+	},
+	key_up : function(e) {
+		for (var i in entities) {
+			if (entities[i].keysDown!==undefined) {
+				delete entities[i].keysDown[e.keyCode];
+			}
+		}
+	},
+	// Level loading and parsing
+	loadLevel : function(levelNum) {
+		// Gets level from server, can't get locally
+		var url = "http://ta2000.github.io/Game/levels/level" + levelNum + ".json";
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (xhttp.readyState == 4) {
+				var json = xhttp.responseText;
+				var obj = {};
+				obj = JSON.parse(json);
+				for (var i = 0; i < obj.board.length; i++) {
+					switch (obj.board[i].type) {
+						case 1:
+							entities['player'] = new Player("images/sprites/player.png", (obj.board[i].x*Game.scale), (obj.board[i].y*Game.scale));
+							break;
+						case 2:
+							entities['entity'+i] = new Goblin("images/sprites/space_goblin.png", (obj.board[i].x*Game.scale), (obj.board[i].y*Game.scale));
+							break;
+						case 3:
+							entities['entity'+i] = new Wall("images/sprites/wall.png", (obj.board[i].x*Game.scale), (obj.board[i].y*Game.scale));
+							break;
+						case 4:
+							entities['entity'+i] = new Portal("images/sprites/portal.png", (obj.board[i].x*Game.scale), (obj.board[i].y*Game.scale));
+							break;
+						default:
+							console.log("Error loading tile: invalid type");
+					}
+				}
+				// Set the view on the player
+				Game.setView(entities.player);
+			}
+		};
+		xhttp.open("GET", url, true);
+		xhttp.send();
+	},
+	setView : function(obj) {
+		var xDif = (Game.canvas.width/2.05 - obj.x)
+		var yDif = (Game.canvas.height/2.3 - obj.y);
+		for (var i in entities) {
+			entities[i].x+=xDif;
+			entities[i].y+=yDif;
+		}
+	}
+}
--- js/class.js
+++ js/class.js
@@ -2,47 +2,29 @@
 
 var entities = {};
 
-class View {
-	constructor() {
-		this.x = 0;
-		this.y = 0;
-	}
-
-	setFollowing( following ) {
-		this.following = following;
-		this.distX = this.x - following.x;
-		this.distY = this.y - following.y;
-	}
-
-	setPos() {
-		if (this.following!=undefined) {
-			this.x = this.following.x + this.distX;
-			this.y = this.following.y + this.distY;
-		}
-	}
-
-	clearScreen( ctx ) {
-		ctx.clearRect(this.x, this.y, Game.canvas.width, Game.canvas.height);
-	}
-}
-
 class Sprite {
 	constructor(img,x,y) {
 		this.image = new Image();
 		this.image.src = img;
 		this.x = x;
 		this.y = y;
+		this.solid = false;
 	}
 
 	draw( ctx ) {
-		if (this.image.src!="") {
-			ctx.drawImage(this.image, this.x, this.y);
-		};
+		ctx.drawImage(this.image, this.x, this.y);
 	}
 
 	collision( obj ) {
-		collided = false;
-		// Working collision detection code
+		var collided = false;
+	
+		if (this.x < obj.x + obj.image.width &&
+			this.x + this.image.width > obj.x &&
+			this.y < obj.y + obj.image.height &&
+			this.image.height + this.y > obj.y) {
+			collided = true;
+		}
+
 		return collided;
 	}
 
@@ -54,29 +36,58 @@ class Player extends Sprite {
 		this.speed = 3;
 		this.keysDown = {};
 		this.viewDist = 10;
+		this.visibleObjs = [];
+	}
+
+	getVisibleObjs() {
+		
 	}
 
 	move(ctx) {
 		/*--W--*/
 		if ( 87 in this.keysDown ) {
-			ctx.translate(0, this.speed);
+			for (var i in entities) { entities[i].y+=this.speed }
 			this.y-=this.speed;
 		};
 		/*--A--*/
 		if ( 65 in this.keysDown ) {
-			ctx.translate(this.speed, 0);
+			for (var i in entities) { entities[i].x+=this.speed }
 			this.x-=this.speed;
 		};
 		/*--S--*/
 		if ( 83 in this.keysDown ) {
-			ctx.translate(0, 0-this.speed);
+			for (var i in entities) { entities[i].y-=this.speed }
 			this.y+=this.speed;
 		};
 		/*--D--*/
 		if ( 68 in this.keysDown ) {
-			ctx.translate(0-this.speed, 0);
+			for (var i in entities) { entities[i].x-=this.speed }
 			this.x+=this.speed;
 		};
+
+		// Collision
+		for (var i in entities) {
+			if (entities[i].solid==true) {
+				if (this.collision(entities[i])) {
+					if ( 87 in this.keysDown ) {
+						for (var i in entities) { entities[i].y-=this.speed; }
+						this.y+=this.speed;
+					};
+					if ( 65 in this.keysDown ) {
+						for (var i in entities) { entities[i].x-=this.speed; }
+						this.x+=this.speed;
+					};
+					if ( 83 in this.keysDown ) {
+						for (var i in entities) { entities[i].y+=this.speed; }
+						this.y-=this.speed;
+					};
+					if ( 68 in this.keysDown ) {
+						for (var i in entities) { entities[i].x+=this.speed; }
+						this.x-=this.speed;
+					};
+				};
+			};
+		}
 	}
 
 }
@@ -92,6 +103,7 @@ class Goblin extends Sprite {
 class Wall extends Sprite {
 	constructor(img,x,y) {
 		super(img,x,y);
+		this.solid = true;
 	}
 }
 
