3886d9db03313d27e2e9a2a748e879490772628b
Alex S <sathler10@gmail.com>
2016-01-29 07:16:51 -0800

    added some small changes

--- js/minimap.js
+++ js/minimap.js
@@ -1,17 +1,19 @@
+var screenRatio = Math.round(document.documentElement.clientHeight + document.documentElement.clientWidth); //would like to use this eventually to be able to have a dynamic minimap scale. Can figure out in the futur.
 var minimap = {
-	x : Math.round(document.documentElement.clientWidth * .10),
-	y : Math.round(document.documentElement.clientWidth * .10),
-	r : Math.round(document.documentElement.clientWidth * .07),
+	x : Math.round(document.documentElement.clientWidth * 0.10),
+	y : Math.round(document.documentElement.clientWidth * 0.10),
+	r : Math.round(document.documentElement.clientWidth * 0.07),
 	scale : 5.5,
-	draw : function(ctx) {
-		// Fill
-		ctx.fillStyle = "#EEEECC";
-		ctx.fill();
-        
+	draw : function (ctx) {
+		'use strict';
         //draws map in minimap
 		for (var i in entities) {
 			entities[i].drawMinimap(ctx);
 		}
+        
+        // Fill
+		ctx.fillStyle = "#EEEECC";
+		ctx.fill();
 
 		// Outline
 		ctx.beginPath();
